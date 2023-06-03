7ae70e6aede460c3d4638a3c48c8dc7a21f83293
shixin <763107756@qq.com>
2023-06-03 12:21:42 +0800

    安装vite压缩插件

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,35 @@ import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import path from 'path'
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
         UnoCSS(),
+        viteCompression(),
         AutoImport({
+            imports: ['vue', 'vue-router', 'pinia'],
             resolvers: [ElementPlusResolver()],
+            dts: false
         }),
         Components({
             resolvers: [ElementPlusResolver()],
         }),
     ],
+    resolve: {
+        alias: {
+            "~": path.resolve(__dirname, './'),
+            "@": path.resolve(__dirname, './src')
+        },
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+    },
+    server: {
+        port: 80,
+        host: true,
+        open: true,
+    }
 })
