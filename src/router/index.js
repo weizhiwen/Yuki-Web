c299a053515a994b8ae3355358798f42c6499fa8
shixin <763107756@qq.com>
2023-06-06 19:07:27 +0800

    添加首页loading、路由进度

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,23 @@
-import {createRouter, createWebHistory} from "vue-router";
-import NProgress from 'nprogress';
+import NProgress from "nprogress";
+import { createRouter, createWebHistory } from "vue-router";
 
 export const constantRouters = [
     {
-        path: '/',
-        component: () => import('@/views/login/index.vue'),
-        hidden: true
+        path: "/",
+        component: () => import("@/views/login/index.vue"),
+        hidden: true,
     },
-]
+];
 
 const router = createRouter({
     history: createWebHistory(),
     routes: constantRouters,
-})
-
+});
+router.beforeEach((to, from, next) => {
+    NProgress.start();
+    next();
+});
+router.afterEach(() => {
+    NProgress.done();
+});
 export default router;
